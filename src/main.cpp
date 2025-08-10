65b57ac7d36a3a0e81c3c09ea70625f621c02a9c
HarounaTraore <167339173+HarounaTraore@users.noreply.github.com>
2025-08-10 12:43:16 +0000

    adding LDR and LED control logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int ldrPin = A0; // LDR sur entrée analogique A0
+int ledPin = 8;  // LED sur sortie numérique 8
+int valeurLDR = 0;
+int seuil = 400; // Ajuste selon ton environnement
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(ledPin, OUTPUT); // LED en sortie
+  Serial.begin(9600);      // Communication série
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  valeurLDR = analogRead(ldrPin); // Lire la luminosité
+  
+  Serial.print("Valeur LDR : ");
+  Serial.println(valeurLDR);
+  
+  if (valeurLDR < seuil) {
+    digitalWrite(ledPin, HIGH); // Allume la LED s'il fait sombre
+  } else {
+    digitalWrite(ledPin, LOW);  // Éteint sinon
+  }
+  
+  delay(500);
 }
